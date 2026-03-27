146f6d4981245ec148a9ca99cade03e68cb242dc
PramisKunwar <pramiskwr123@gmail.com>
2026-03-27 09:11:37 +0545

    Search by auto id

--- search.c
+++ search.c
@@ -1,33 +1,54 @@
 #include "student.h"
 
 void searchRecord()
-    {
-        FILE *fptr;
+{
+    FILE *fptr;
     struct student_db s;
+    int id, found = 0;
+    char input[MAX_INPUT];
 
-    fptr = fopen("student.txt","r");
+    fptr = fopen("student.txt", "r");
 
-    if(fptr==NULL)
+    if (fptr == NULL)
     {
-        printf("File not found\n");
+        printf("\nNo records found. Database is empty.\n");
         return;
     }
-    int id;
-    printf("Enter student id to search ");
-    scanf("%d",&id);
 
-    printf("\n\tSTUDENT DETAILS");
-    printf("\n=================================================\n");
-    printf("ID\tNAME\t\tAGE\t\tMARKS\n");
+    printf("Enter student ID to search: ");
+    fgets(input, MAX_INPUT, stdin);
+    id = atoi(input);
 
-    while(fscanf(fptr,"%d %s %d %f",&s.id,s.name,&s.age,&s.marks)!=EOF)
+    if (id <= 0)
     {
-        if(s.id == id)
+        printf("Error: Invalid ID!\n");
+        fclose(fptr);
+        return;
+    }
+
+    printf("\n\tSTUDENT DETAILS\n");
+    printf("=================================================\n");
+    printf("ID\tNAME\t\t\tAGE\tMARKS\n");
+    printf("=================================================\n");
+
+    while (fscanf(fptr, "%d %49s %d %f", &s.id, s.name, &s.age, &s.marks) == 4)
+    {
+        if (s.id == id)
         {
-        printf("%d\t%s\t\t%d\t\t%.2f\n",s.id,s.name,s.age,s.marks);
+            printf("%-5d %-20s %-8d %.2f\n", s.id, s.name, s.age, s.marks);
+            found = 1;
+            break;
         }
     }
-    
 
     fclose(fptr);
+
+    if (!found)
+    {
+        printf("\nRecord with ID %d not found!\n", id);
     }
+    else
+    {
+        printf("=================================================\n");
+    }
+}
